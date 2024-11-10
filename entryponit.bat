6a5fe81d81bebd27099d37b6c051bb875f75efdd
Zhengxin <1045418215@qq.com>
2024-11-10 22:20:18 +0800

    revise bat

--- entryponit.bat
+++ entryponit.bat
@@ -1,3 +1,15 @@
 @echo off
-python %FOBSC_PATH%/run_interface.py  % 替换为run_interface.py的实际路径
-pause
+:: 设置 conda 的基本路径
+SET "CONDA_BASE=D:\anaconda3"  
+
+:: 手动调用 activate.bat 脚本以初始化 conda 环境
+CALL "%CONDA_BASE%\Scripts\activate.bat" "%CONDA_BASE%"
+
+:: 激活指定的虚拟环境
+CALL conda activate fob_kilosort
+
+:: 运行 Python 脚本
+python F:\#Gong\Lab\FOBCheck\run_interface.py 
+
+:: 保持窗口打开以查看错误信息
+:: pause
